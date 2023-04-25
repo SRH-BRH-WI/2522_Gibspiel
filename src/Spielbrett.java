ccb60a7355d31506e5f60f3036657fc404781dcf
Dr. Dillinger <peter.dillinger@srh.de>
2023-04-25 09:15:55 +0200

    Skelett vom Spielbrett hinzugefügt.

---
+++ src/Spielbrett.java
@@ -0,0 +1,25 @@
+public class Spielbrett {
+    private int anzahlElemente;
+
+    public Spielbrett(int anzahlElemente) {
+        this.anzahlElemente = anzahlElemente;
+    }
+
+    public int getAnzahlElemente() {
+        return anzahlElemente;
+    }
+
+    public boolean elementeHinzufügen(int anzahlElemente) {
+        // TODO
+        return false;
+    }
+
+    public void spielAufbau() {
+        // TODO
+    }
+
+    public boolean istSpielZuEnde() {
+        // TODO
+        return false;
+    }
+}
